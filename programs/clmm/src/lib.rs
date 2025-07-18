9e007f61f99b511509d3ae0ba639d363a8781071
alxn787 <alenabraham787@gmail.com>
2025-07-18 12:35:30 +0530

    states

--- programs/clmm/src/lib.rs
+++ programs/clmm/src/lib.rs
@@ -6,11 +6,37 @@ declare_id!("4GhrgMYusqS5uuyzrrBvFv3FuVGp4RRp4XKDBctyW6oN");
 pub mod clmm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializePool>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializePool {
+
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Pool {
+    pub liquidity: u128,
+    pub sqrt_price_x96 : u128,
+    pub current_tick :i32,
+    pub tick_spacing : i32
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Tick {
+    pub liquidity_gross: u128,
+    pub initialized: bool,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Position {
+    pub liquidity: u128,
+    pub tick_lower: i32,
+    pub tick_upper: i32,
+}
