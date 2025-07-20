ca833611a22342d06ce1c46fb04e7c5233685bad
alxn787 <alenabraham787@gmail.com>
2025-07-20 10:15:18 +0530

    burn instruction

--- programs/clmm/src/lib.rs
+++ programs/clmm/src/lib.rs
@@ -155,6 +155,52 @@ pub struct Swap <'info> {
     pub rent: Sysvar<'info, Rent>,
 }
 
+#[derive(Accounts)]
+#[instruction(lower_tick:i32, upper_tick:i32, liquidity_amount: u128)]
+pub struct Burn<'info> {
+
+    #[account(mut)]
+    pub pool: Account<'info, Pool>,
+
+    #[account(
+        mut,
+        close = owner,
+        seeds = [
+            b"position",
+            owner.key().as_ref(),
+            pool.key().as_ref(),
+            &lower_tick.to_le_bytes(),
+            &upper_tick.to_le_bytes(),
+        ],
+        bump = position.bump,
+    )]
+    pub position: Account<'info, Position>,
+
+    #[account(mut)]
+    pub tick_array_lower: Account<'info, TickArray>,
+    
+    #[account(mut)]
+    pub tick_array_upper: Account<'info, TickArray>,
+
+    #[account(mut)]
+    pub pool_token_0: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub pool_token_1: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub user_token_0: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub user_token_1: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct Pool {
@@ -176,6 +222,9 @@ pub struct Position {
     pub liquidity: u128,
     pub tick_lower: i32,
     pub tick_upper: i32,
+    pub owner: Pubkey,
+    pub pool: Pubkey,
+    pub bump: u8,
 }
 
 #[account]
