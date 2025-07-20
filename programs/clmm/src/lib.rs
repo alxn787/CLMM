2a52e8877b1b7423e406151add16c5b13d33ba45
alxn787 <alenabraham787@gmail.com>
2025-07-20 08:49:15 +0530

    initpool

--- programs/clmm/src/lib.rs
+++ programs/clmm/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("4GhrgMYusqS5uuyzrrBvFv3FuVGp4RRp4XKDBctyW6oN");
 
@@ -13,9 +14,51 @@ pub mod clmm {
 }
 
 #[derive(Accounts)]
-pub struct InitializePool {
+#[instruction(tick_spacing: i32)]
+pub struct InitializePool<'info> {
 
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = Pool::INIT_SPACE,
+        seeds = [
+            b"pool".as_ref(),
+            token_mint_0.key().as_ref(),
+            token_mint_1.key().as_ref(),
+            &tick_spacing.to_le_bytes()
+        ],
+        bump,
+    )]
+    pub pool: Account<'info, Pool>,
+    pub token_mint_0: Account<'info, Mint>,
+    pub token_mint_1: Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = token_mint_0,
+        token::authority = pool,
+    )]
+    pub token_vault_0: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = token_mint_1,
+        token::authority = pool,
+    )]
+    pub token_vault_1: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent : Sysvar<'info,Rent>
 }
+
+
+
 
 #[account]
 #[derive(InitSpace)]
